0c68b3e7d6b8c75f75b0dd69eddd73269c87f358
Emilio Martinez-Cordero <e.marticor@gmail.com>
2017-10-31 15:12:01 -0700

    refactor: allow `any` regardless of value
    BREAKING CHANGE: before `null` would not be considered valid under `any`, but after sleeping on this for a while I believe `any` should literally just be the catch all for ‘any’ type.
    
    Additionally, this brings a huge perf boost for when `any` is the type being passed.

--- src/is.internal.ts
+++ src/is.internal.ts
@@ -84,77 +84,76 @@ export function isPrimitive(val: any): boolean {
  * Tests an object against an object schema.
  */
 export function matchesSchema(val: any, schema: isTypeSchema | isTypeSchema[]): boolean {
-  return (
-    (Array.isArray(schema) ? schema : [schema])
-      /** Test every schema until at least one of them matches */
-      .some((s: isTypeSchema) => {
-        /** If type is defined but invalid, schema is false */
-        if (s.type !== undefined && !validDataType(s.type)) return false
+  const schemas = Array.isArray(schema) ? schema : [schema];
 
-        /** Cache the type. Use `any` if none is present */
-        const sType: DataType | DataType[] = s.type === undefined ? <DT>DATATYPE.any : s.type
+  /** Test every schema until at least one of them matches */
+  return schemas.some((s: isTypeSchema) => {
+    /** If type is defined but invalid, schema is false */
+    if (s.type !== undefined && !validDataType(s.type)) return false
 
-        /** Test if any of the data types matches */
-        const sTypeValid = isOneOfMultipleTypes(val, sType, s.options)
+    /** Cache the type. Use `any` if none is present */
+    const sType: DataType | DataType[] = s.type === undefined ? <DT>DATATYPE.any : s.type
 
-        /**
-         * Whether the properties match what's reflected in the schema.
-         * Initially assumed as `true`.
-         */
-        let sPropsValid = true
+    /** Test if any of the data types matches */
+    const sTypeValid = isOneOfMultipleTypes(val, sType, s.options)
 
-        /**
-         * Whether the required properties are present.
-         * Initially assumed as `true`.
-         */
-        let sRequiredValid = true
+    /**
+     * Whether the properties match what's reflected in the schema.
+     * Initially assumed as `true`.
+     */
+    let sPropsValid = true
 
-        /** Extract the properties to test for into an array */
-        const sProps = s.props && typeof s.props === 'object' ? s.props : {};
-        const sPropKeys: string[] = Object.keys(sProps)
+    /**
+     * Whether the required properties are present.
+     * Initially assumed as `true`.
+     */
+    let sRequiredValid = true
 
-        /** Begin tests relevant to properties */
-        if (sPropKeys.length > 0) {
-          /**
-           * Get all keys that are required from the schema,
-           * and then test for required properties.
-           */
-          sRequiredValid = sPropKeys
-            .filter(p => sProps[p].required === true)
-            .every(r => val[r] !== undefined)
+    /** Extract the properties to test for into an array */
+    const sProps = s.props && typeof s.props === 'object' ? s.props : {};
+    const sPropKeys: string[] = Object.keys(sProps)
 
-          /**
-           * Iterate over the property keys.
-           *
-           * If the subject has the property we're seeking,
-           * `matchesSchema` is called on that property.
-           *
-           * If `p`, the property, is not an object, it won't be validated against.
-           * However, if it was required, that will have been caught by the check above.
-           */
-          sPropsValid = sPropKeys.every(p =>
-            !!s.props && val !== undefined && val[p] !== undefined
-              ? matchesSchema(val[p], s.props[p])
-              : true
-          )
-        }
+    /** Begin tests relevant to properties */
+    if (sPropKeys.length > 0) {
+      /**
+       * Get all keys that are required from the schema,
+       * and then test for required properties.
+       */
+      sRequiredValid = sPropKeys
+        .filter(p => sProps[p].required === true)
+        .every(r => val[r] !== undefined)
 
-        /** If `type` is Any, check whether value is array. If so, check items */
-        const inferredArray = sType === <DT>DATATYPE.any && Array.isArray(val)
+      /**
+       * Iterate over the property keys.
+       *
+       * If the subject has the property we're seeking,
+       * `matchesSchema` is called on that property.
+       *
+       * If `p`, the property, is not an object, it won't be validated against.
+       * However, if it was required, that will have been caught by the check above.
+       */
+      sPropsValid = sPropKeys.every(p =>
+        !!s.props && val !== undefined && val[p] !== undefined
+          ? matchesSchema(val[p], s.props[p])
+          : true
+      )
+    }
 
-        /**
-         * Whether Array items are valid
-         * If we're dealing with an array, even if inferred, check the `items`. Otherwise, true.
-         */
-        const sItemsValid = (
-          (sType === <DT>DATATYPE.array || inferredArray) && sTypeValid && s.items !== undefined
-            ? (val as any[]).every(i => matchesSchema(i, s.items as isTypeSchema | isTypeSchema[]))
-            : true
-        );
+    /** If `type` is Any, check whether value is array. If so, check items */
+    const inferredArray = sType === <DT>DATATYPE.any && Array.isArray(val)
 
-        return sTypeValid && sRequiredValid && sPropsValid && sItemsValid
-      })
-  )
+    /**
+     * Whether Array items are valid
+     * If we're dealing with an array, even if inferred, check the `items`. Otherwise, true.
+     */
+    const sItemsValid = (
+      (sType === <DT>DATATYPE.array || inferredArray) && sTypeValid && s.items !== undefined
+        ? (val as any[]).every(i => matchesSchema(i, s.items as isTypeSchema | isTypeSchema[]))
+        : true
+    );
+
+    return sTypeValid && sRequiredValid && sPropsValid && sItemsValid
+  })
 }
 
 /**
@@ -169,9 +168,7 @@ export function isOneOfMultipleTypes(val: any, type: DataType | DataType[], opti
    * Else if `types` contain any, return true
    * Else test against `is`
    */
-  return types.length > 0
-    ? types.indexOf(<DT>DATATYPE.any) >= 0 || types.some(n => is(val, n, options))
-    : false
+  return types.length > 0 ? types.some(n => is(val, n, options)) : false
 }
 
 /**
