78275193bc2aeb5ff9ca31e5a4ec3b26a7ba0247
Emilio Martinez <emilio-martinez@users.noreply.github.com>
2018-05-12 18:04:05 -0700

    fix(multipleof): validate value is number (#148)

--- src/number-helpers.ts
+++ src/number-helpers.ts
@@ -5,20 +5,14 @@
  */
 export function isMultipleOf(val: number | undefined, multipleOf: number | undefined): boolean {
   return (
-    multipleOf === 0 ||
-    (typeof val === 'number' &&
-      /**
-       * The modulus operator here excludes both infinities, i.e.,
-       * isNaN(Infinity % 1) === true
-       */
-      !isNaN(val % 1) &&
-      /**
-       * `Math.abs` avoids `-0`
-       * The non-null assertion below is okay because we're
-       * strictly checking for the remainder to be zero
-       */
-      // tslint:disable-next-line no-non-null-assertion
-      Math.abs(val % multipleOf!) === 0)
+    // First, Check that val is a number
+    typeof val === 'number' &&
+    // A few things going on here:
+    // * OK if multipleOf is 0; all numbers are multiples of zero, i.e., x * 0 === 0
+    // * isNaN check uses modulus operator to exclude infinities, i.e., isNaN(Infinity % 1) === true
+    // * Math.abs` avoids `-0`
+    // tslint:disable-next-line no-non-null-assertion
+    (multipleOf === 0 || (!isNaN(val % 1) && Math.abs(val % multipleOf!) === 0))
   );
 }
 
