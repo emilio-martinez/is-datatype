2e975c221f4d9d18e38bf764a156493de279e753
Emilio Martinez <e.marticor@gmail.com>
2019-10-07 02:31:06 -0700

    refactor(is): untangle options assignment

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -25,9 +25,11 @@ function validSchema(val: any) {
   );
 }
 
-const stringParamRegex = /^(pattern|patternFlags)$/;
-const booleanParamRegex = /^(exclEmpty|arrayAsObject)$/;
-const numberParamRegex = /^(min|max|multipleOf)$/;
+type AssignIfTest = (v: any) => boolean;
+
+const validStr: AssignIfTest = v => typeof v === 'string';
+const validBool: AssignIfTest = v => typeof v === 'boolean';
+const validNum: AssignIfTest = v => typeof v === 'number' && !isNaN(v);
 
 export class Options implements StrictOptions {
   // from StrictOptionsObject
@@ -46,24 +48,25 @@ export class Options implements StrictOptions {
   // from StrictOptionsArray
   readonly type: DataType | DataType[] = <DT>DATATYPE.any;
 
-  constructor(options: isOptions | null | undefined) {
-    if (options === null || options === undefined) return;
+  constructor(opts: isOptions | null | undefined) {
+    if (opts === null || opts === undefined) return;
 
-    for (const k in options) {
-      const val = options[<keyof isOptions>k];
-      if (this.hasOwnProperty(k) && val !== undefined) {
-        if (
-          (<keyof isOptions>k === 'type' && validMultiDataType(<isOptions['type']>val)) ||
-          (typeof val === 'string' && stringParamRegex.test(k)) ||
-          (typeof val === 'boolean' && booleanParamRegex.test(k)) ||
-          (typeof val === 'number' && !isNaN(val) && numberParamRegex.test(k)) ||
-          (<keyof isOptions>k === 'schema' && validSchema(<isOptions['schema']>val))
-        ) {
-          // TODO: figure out why this started being a mismatch as of TS 3.5.2
-          (this[<keyof isOptions>k] as any) = val;
-        } else {
-          throw Error(`Invalid option ${k} provided: ${JSON.stringify(options)}`);
+    this._assignIf(validMultiDataType, opts, 'type');
+    this._assignIf(validStr, opts, ['pattern', 'patternFlags']);
+    this._assignIf(validBool, opts, ['exclEmpty', 'arrayAsObject']);
+    this._assignIf(validNum, opts, ['min', 'max', 'multipleOf']);
+    this._assignIf(validSchema, opts, 'schema');
+  }
+
+  private _assignIf<K extends keyof this>(test: AssignIfTest, opts: isOptions, keys: K | K[]) {
+    for (const k of ([] as K[]).concat(keys)) {
+      const val = opts[<keyof isOptions>k] as this[K] | undefined;
+
+      if (val !== undefined) {
+        if (!test(val)) {
+          throw Error(`Invalid option provided: ${k}`);
         }
+        this[k] = val;
       }
     }
   }
